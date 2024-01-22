43746a170ca76f0ef2664371aafb5273d4c223d0
Roman Hornak <ro.hornak@gmail.com>
2024-01-22 08:49:30 +0100

    gen

--- example/lib/modules/modula_a/modula_a.dart
+++ example/lib/modules/modula_a/modula_a.dart
@@ -1,6 +1,12 @@
+library modula_a;
+
+import 'modula_a.dart';
+
 export 'controls/abc.dart';
 export 'controls/mod_control.dart';
 export 'entities/mod_entity.dart';
 export 'presentation/mod_state.dart';
 export 'presentation/mod_ui.dart';
 export 'repositories/mod_repo.dart';
+
+part 'modula_a_provider.dart';

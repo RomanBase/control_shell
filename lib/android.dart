a855b94a7c1361af3f3cfe70581fb6a3d7b7ccb0
Roman Hornak <ro.hornak@gmail.com>
2025-04-16 11:46:28 +0200

    manifest

--- lib/android.dart
+++ lib/android.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:archive/archive.dart';
 import 'package:control_shell/shell.dart';
 import 'package:googleapis/androidpublisher/v3.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart';
+import 'package:xml/xml.dart';
 
 ///
 /// https://developers.google.com/android-publisher
@@ -31,7 +30,7 @@ Future<EditsResource> _edits(String serviceAccount) async {
 }
 
 Future<Map<String, dynamic>> uploadAppBundle(ControlShell shell, {String? serviceAccount, String? packageName}) async {
-  packageName ??= await _packageNameFromArchive(shell);
+  packageName ??= await packageNameFromArchive(shell);
   serviceAccount ??= (await LocalConfig.read()).googleService;
 
   if (serviceAccount == null) {
@@ -60,7 +59,7 @@ Future<Map<String, dynamic>> uploadAppBundle(ControlShell shell, {String? servic
 
 //https://developers.google.com/android-publisher/tracks#ff-track-name
 Future<Map<String, dynamic>> publish(ControlShell shell, {String? serviceAccount, String? packageName, String track = 'alpha'}) async {
-  packageName ??= await _packageNameFromArchive(shell);
+  packageName ??= await packageNameFromArchive(shell);
   final buildNumber = (await LocalConfig.read()).buildNumber;
   serviceAccount ??= (await LocalConfig.read()).googleService;
 
@@ -98,14 +97,22 @@ Future<ServiceAccountCredentials> getGoogleServiceCredentials(String serviceAcco
   return ServiceAccountCredentials.fromJson(json);
 }
 
-Future<String> _packageNameFromArchive(ControlShell shell) async {
-  final bytes = File(path(shell.rootShell().path, [_path])).readAsBytesSync();
-  final archive = ZipDecoder().decodeBytes(bytes);
-  final manifest = archive.firstWhere((value) => value.name.endsWith('AndroidManifest.xml'));
-  final content = String.fromCharCodes(manifest.content);
+Future<XmlDocument> manifestFromArchive(String dir, {String abb = 'app-release.aab', String bundletool = 'bundletool.jar'}) async {
+  final aabPath = '$dir/$abb';
 
-  //just quickly read package name - we don't need to decompress manifest.
-  final declaration = LineSplitter.split(content).elementAt(4);
+  final result = await Process.run('java', ['-jar', bundletool, 'dump', 'manifest', '--bundle=$aabPath']);
 
-  return declaration.split('"')[0];
+  return XmlDocument.parse(result.stdout);
+}
+
+Future<String> packageNameFromArchive(ControlShell shell, {String? dir, String abb = 'app-release.aab', String bundletool = 'bundletool.jar'}) async {
+  dir ??= path(shell.rootShell().path, [
+    path('build', ['app', 'outputs', 'bundle', 'release'])
+  ]);
+
+  final xml = await manifestFromArchive(dir, abb: abb, bundletool: bundletool);
+
+  print(xml.getElement('manifest')?.getAttribute('package'));
+
+  return 'end';
 }
